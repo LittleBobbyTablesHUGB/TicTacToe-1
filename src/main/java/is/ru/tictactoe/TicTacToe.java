42c4161e04ccc505625e77cf076a2dff86d0821c
Hlif <hlifh15@ru.is>
2016-10-25 22:13:44 +0000

    Revert "Added movePlayer and test cases for that"

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,44 +1,9 @@
 package is.ru.tictactoe;
 
-import static java.lang.System.out;
-import java.util.*;
 
 public class TicTacToe {
-	public char[][] gameBoard;
-	public char[] playerMark;
-	public int currentPlayer;
-
-	public TicTacToe() {
-		//Initialize the game
-		initializeGame();
-	}
-
-	private void initializeGame() {
-		//Set the players marks to X and O
-		playerMark = new char[2];
-		playerMark[0] = 'X';
-		playerMark[1] = 'Y';
-		//Set the current player as 0(X)
-		currentPlayer = 0;
-		//Create and empty game board
-		gameBoard = new char[3][3];
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
-				gameBoard[i][j] = ' ';
-			}
-		}
-	}
-
-	public void playerMove(int row, int col){
-		//check if the move that the player move is legal
-		if(col < 0 || col > 2 || row > 2 || row < 0 || gameBoard[row][col] != ' '){
-			System.out.println("Illegal move please try again!");
-		}
-		else{
-			//Mark the player symbol on the board
-			gameBoard[row][col] = playerMark[currentPlayer];
-			//Switch players
-			currentPlayer = 1-currentPlayer;
-		}
-	}	
+    public static void main(String[] args) {
+    	World world = new World();    
+    	System.out.println(world.greet());
+    }
 }
--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -1,26 +1,14 @@
 package is.ru.tictactoe;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static java.lang.System.out;
 
 public class TicTacToeTest {
 
-	
-	@Test
-	public void testPlayerMove() {
-		TicTacToe game = new TicTacToe();
-		game.playerMove(2,2);
-		assertEquals(game.playerMark[1 - game.currentPlayer], game.gameBoard[2][2]); 
-	}
-	
-	@Test
-	public void testIllegaMove() {
-		TicTacToe game = new TicTacToe();
-		int prevPlayer = game.currentPlayer;
-		game.playerMove(2,3);
-		int afterMovePlayer = game.currentPlayer;
-		//If move was illegal same player should be playing
-		assertEquals(prevPlayer, afterMovePlayer);
-	}
+    @Test
+    public void greetResultsInHello() {
+        World world = new World();
+        assertEquals("Hello world!", world.greet()); }
+
 }

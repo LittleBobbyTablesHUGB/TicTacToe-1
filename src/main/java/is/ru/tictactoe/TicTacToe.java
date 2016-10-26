28f2c48d8e950d63a362661477ce12685c717a58
Lára Valdís Kristjánsdóttir <lara15@i-1217c9a8.cs1004cloud.internal>
2016-10-26 20:20:02 +0000

    made 5 testWinner testcases

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -18,7 +18,7 @@ public class TicTacToe {
 		//Set the players marks to X and O
 		playerMark = new char[2];
 		playerMark[0] = 'X';
-		playerMark[1] = 'Y';
+		playerMark[1] = 'O';
 		//Set the current player as 0(X)
 		currentPlayer = 0;
 	}
--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -28,6 +28,65 @@ public class BoardTest{
                 game.gameBoard.drawGameBoard();
                 assertEquals(1,1);
         }
+	
+	@Test
+	public void TestWinner() {
+		TicTacToe game = new TicTacToe();
+		assertEquals(false,game.gameBoard.isWinner());
+	}
+	
+	@Test
+	public void TestWinner1() {
+		TicTacToe game = new TicTacToe();
+		game.playerMove(0,0);
+		game.playerMove(1,1);
+		game.playerMove(0,1);
+		game.playerMove(2,2);
+		game.playerMove(0,2);
+		assertEquals(true,game.gameBoard.isWinner());	
+	}
+        @Test
+        public void TestWinner2() {
+                TicTacToe game = new TicTacToe();
+                game.playerMove(0,0);
+                game.playerMove(1,0);
+                game.playerMove(1,1);
+                game.playerMove(1,2);
+                game.playerMove(2,2);
+                assertEquals(true,game.gameBoard.isWinner());
+        }
 
+	@Test
+        public void TestWinner3() {
+                TicTacToe game = new TicTacToe();
+                game.playerMove(0,0);
+                game.playerMove(1,0);
+                game.playerMove(1,1);
+                game.playerMove(1,2);
+                game.playerMove(2,2);
+                assertEquals(true,game.gameBoard.isWinner());
+        }
 
+    	@Test
+        public void TestWinner4() {
+                TicTacToe game = new TicTacToe();
+		game.playerMove(2,0);
+                game.playerMove(1,0);
+                game.playerMove(2,1);
+                game.playerMove(1,1);
+                game.playerMove(2,2);
+                assertEquals(true,game.gameBoard.isWinner());
+	}
+
+   	@Test
+        public void TestWinner5() {
+                TicTacToe game = new TicTacToe();
+                game.playerMove(0,1);
+                game.playerMove(0,2);
+                game.playerMove(2,1);
+                game.playerMove(1,1);
+                game.playerMove(1,2);
+		game.playerMove(2,0);
+		assertEquals(true,game.gameBoard.isWinner());
+        }
 }

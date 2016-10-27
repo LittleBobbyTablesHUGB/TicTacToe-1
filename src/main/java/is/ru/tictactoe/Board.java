a7428f4d1def1ee4c4248e7c22ec20fb9cd7423f
Bríet Konráðsdóttir <briet15@i-1217c9a8.cs1004cloud.internal>
2016-10-27 15:11:54 +0000

    travis fix

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -76,5 +76,23 @@ public class Board{
 
         }
 
+	public boolean isDraw(){
+
+                if(!isWinner()){
+                        for (int i = 0; i < 3; i++) {
+                                for (int j = 0; j < 3; j++) {
+                                        if (board[i][j] != 'X' && board[i][j] != 'O') {
+                                                return false;
+                                        }
+                                }
+                        }
+                        return true;
+                }
+                else{
+                        return false;
+                }
+        }
+
+
 }
 

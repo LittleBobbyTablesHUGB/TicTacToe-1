4b47016ff3c7e1812f45c431acbab38b4aa6031e
Hildur Björg Gunnarsdóttir <hildurg09@i-1217c9a8.cs1004cloud.internal>
2016-10-28 22:25:04 +0000

    added draw, winner and functionality for the button

--- src/main/resources/public/scripts/tttScript.js
+++ src/main/resources/public/scripts/tttScript.js
@@ -7,18 +7,65 @@ $( document ).ready(function() {
           url: "/id",
           data: "id=" + boxId
       }).done(function(result) {
-          if (result === "X") {
+	  if (result === "X") {
               	var d = document.getElementById(boxId);
 		d.className = "cross";
 		$("#" + boxId).html(result);
+		$("#whosTurn").html("Player O´s turn");
           }
 	  else if(result === "O"){
 		var d = document.getElementById(boxId);
 		d.className = "circle";
 		$("#" + boxId).html(result);
+		$("#whosTurn").html("Player X´s turn");
+	  }
+	  else if(result == "0"){
+		var d = document.getElementById(boxId);
+                d.className = "cross";
+                $("#" + boxId).html("X");
+		$("#whosTurn").html("Its a draw!");
+	  }
+	 else if(result == "3"){
+                var d = document.getElementById(boxId);
+                d.className = "circle";
+                $("#" + boxId).html("O");
+                $("#whosTurn").html("Its a draw!");
+          }
+	  else if(result == "1"){
+		var d = document.getElementById(boxId);
+                d.className = "cross";
+		$("#" + boxId).html("X"); 
+		$("#whosTurn").html("X is the winner!");
+	  }
+	  else if(result == "2"){
+		var d = document.getElementById(boxId);
+                d.className = "circle";
+		$("#" + boxId).html("O");
+		$("#whosTurn").html("O is the winner!");
+	  }
+	  else{
+		 $("#whosTurn").html("An error occured, please try again or restart the game!");
 	  }
       });
       event.preventDefault();
     };
     });
 });
+
+function clearBoard(){
+  for(var i = 1; i < 10; i++){
+    $("#" + i).html("");
+  }
+}
+
+function playGame() {
+  $.ajax({
+      type: "post",
+      url: "/playGame",
+  }).done(function(result) {
+      if(result) {
+	  $("#whosTurn").html("Player X´s turn");
+          clearBoard();
+      }
+  });
+}

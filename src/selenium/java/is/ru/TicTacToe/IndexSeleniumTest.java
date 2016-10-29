276f821d3192ffcd5fd6a656f2d6051db6096180
Harpa Guðjónsdóttir <harpa09@i-1217c9a8.cs1004cloud.internal>
2016-10-29 23:49:36 +0000

    simplified

--- src/selenium/java/is/ru/TicTacToe/IndexSeleniumTest.java
+++ src/selenium/java/is/ru/TicTacToe/IndexSeleniumTest.java
@@ -9,13 +9,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import java.net.URL;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import java.net.URL;
 
-public class IndexSeleniumTest{
+public abstract class IndexSeleniumTest{
 
 	static ChromeDriver driver;
 	static String baseUrl;
